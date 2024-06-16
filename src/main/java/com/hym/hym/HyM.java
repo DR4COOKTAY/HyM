e9ed334d16695fb4187045cf42318fa856a5a8d4
Marko <Marko@DESKTOP-RS86C8A>
2024-06-16 00:41:59 -0500

    Prueba

---
+++ src/main/java/com/hym/hym/HyM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hym.hym;
+
+/**
+ *
+ * @author Marko
+ */
+public class HyM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
